1f963488d000020aa757fcf521ba1a25aec139a8
daiki_win <kubotadaiki0654@gmail.com>
2023-11-22 18:48:02 +0900

    feat: csvの出力方法などを改良

--- src/get_metadata.py
+++ src/get_metadata.py
@@ -5,64 +5,48 @@ import pandas as pd
 import os
 import datetime
 import time
+from pathlib import Path
+import json
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-meta_path = "../data/metadata.csv"
+data_path = Path("../data")
+meta_path = data_path / "metadata"
 url = "https://api.edinet-fsa.go.jp/api/v2/documents.json"
 
-columns = [
-    "seqNumber",
-    "docID",
-    "edinetCode",
-    "secCode",
-    "JCN",
-    "filerName",
-    "fundCode",
-    "ordinanceCode",
-    "formCode",
-    "docTypeCode",
-    "periodStart",
-    "periodEnd",
-    "submitDateTime",
-    "docDescription",
-    "issuerEdinetCode",
-    "subjectEdinetCode",
-    "subsidiaryEdinetCode",
-    "currentReportReason",
-    "parentDocID",
-    "opeDateTime",
-    "withdrawalStatus",
-    "docInfoEditStatus",
-    "disclosureStatus",
-    "xbrlFlag",
-    "pdfFlag",
-    "attachDocFlag",
-    "englishDocFlag",
-    "csvFlag",
-    "legalStatus",
-]
-# pd.DataFrame(columns=columns).to_csv(meta_path, encoding="utf_8_sig", index=None)
 
-start = datetime.datetime.strptime("2021-12-07", "%Y-%m-%d")
-end = datetime.datetime.strptime("2022-12-07", "%Y-%m-%d")
+with open(data_path / "metadata_param.json", encoding="utf-8") as f:
+    param = json.load(f)
+
+start = datetime.datetime.strptime(param["start"], "%Y-%m-%d")
+end = datetime.datetime.strptime(param["end"], "%Y-%m-%d")
+
+
 # %%
-for date_time in pd.date_range(start, end):
+for date in pd.date_range(start, end):
     payload = {
-        "date": date_time.date(),
+        "date": date.date(),
         "type": "2",
         "Subscription-Key": API_KEY,
     }
 
     response = requests.get(url, params=payload).json()
-    pd.DataFrame(response["results"]).to_csv(
-        meta_path,
-        mode="a",
-        header=False,
-        index=None,
-        encoding="utf_8_sig",
-    )
+
+    print(date.date())
+    if response["metadata"]["status"] == "404":
+        print(f"[404] 日付がapi対象範囲外だと考えられます\n")
+        continue
+
+    results = response["results"]
+    if results == []:
+        print(f"[200] レスポンスが空なため、書き込まれませんでした\n")
+    else:
+        pd.DataFrame(results).to_csv(
+            meta_path / f"{date.date()}.csv",
+            index=None,
+            encoding="utf_8_sig",
+        )
+        print(f"[200] 正常に書き込まれました\n")
     time.sleep(0.5)
-    print(date_time.date())
 # %%
